cc814c24087e22dcca3da199c1f3a1f0f369afdc
ajaikumar1991 <kb.ajaikumar1991@gmail.com>
2024-02-01 15:48:47 +0530

    commit

--- tests/Playwright Training/Ex5.spec.js
+++ tests/Playwright Training/Ex5.spec.js
@@ -41,13 +41,32 @@ test('Select Multiple Drop Down', async ({ page }) => {
     //Assertions
     //1) Check no of options in drop down - approach1
 
-    const options=await page.locator('#multiple-select-example')
-    await expect(options).toHaveCount(3)
+    const options=await page.locator('#multiple-select-example option');
+    await expect(options).toHaveCount(3);
 
     //Approach 2
 
-    const optionNew=await page.$$('#multiple-select-example')
-    await expect(optionNew.length).toBe(2)
+    const optionNew=await page.$$('#multiple-select-example option');
+    await expect(optionNew.length).toBe(3);
+
+
+     //2)Check presence of value in the dropdown - Approach 1
+     const content=await page.locator('#multiple-select-example').textContent(); // to get text content from the dropdown in the form of string
+     await expect(content.includes('Apple')).toBeTruthy();
+     await expect(content.includes('Bike')).toBeFalsy();
+
+
+        //Aproach 2 -using looping statement
+    const textContent=await page.$$('#multiple-select-example')
+    let status=false
+    for(const option of textContent){
+        let value=await option.textContent()
+        if(value.includes('Orange')){
+            status=true
+            break
+        }
+    }
+    expect(status).toBeTruthy()
 
     await page.close();
 
