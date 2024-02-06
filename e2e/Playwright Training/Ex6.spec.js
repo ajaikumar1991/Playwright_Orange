1aa9caa7dbbae5cbb6735e96fc732c1f8e33afa1
ajaikumar1991 <kb.ajaikumar1991@gmail.com>
2024-02-06 19:35:52 +0530

    commit

--- e2e/Playwright Training/Ex6.spec.js
+++ e2e/Playwright Training/Ex6.spec.js
@@ -1,10 +1,9 @@
 //@ts-check
 import { test, expect } from '@playwright/test'
 
-test.only('Mouse Hover', async function ({ page }) {
+test('Mouse Hover', async function ({ page }) {
 
     await page.goto("https://demo.opencart.com/");
-    //const newLocal = "//a[normalize-space()='Macs (0)']";
     await page.locator("//a[normalize-space()='Desktops']").hover()
     await page.locator("//a[normalize-space()='Mac (1)']").hover()
     await page.locator("//a[normalize-space()='Mac (1)']").click()
@@ -19,14 +18,3 @@ test('Right Click', async ({ page }) => {
   await rightClick.click({button:'right'});
 await page.locator('li').filter({ hasText: 'Quit' }).click();
 });
-
-
-test('Mouse Double Click',async function({page}){
-
-    await page.goto("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick3")
-    await page.waitForTimeout(10000);
-    const buttonLocator = page.locator("//button[normalize-space()='Copy Text']")
-    buttonLocator.dblclick(); 
-    await expect(page.locator("//input[@id='field2']")).toHaveText("Hello World!");
-    await page.waitForTimeout(3000);
-})
