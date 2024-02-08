539b1aa91915e54cfc932f9898ecb880d9371883
ajaikumar1991 <kb.ajaikumar1991@gmail.com>
2024-02-08 14:37:35 +0530

    Commit

--- tests/Playwright Training/Ex9.spec.js
+++ tests/Playwright Training/Ex9.spec.js
@@ -1,5 +1,31 @@
 import { test, expect } from '@playwright/test';
 
+test('Print Tables in Pagination', async ({ page }) => {
+  await page.goto('https://demo.opencart.com/admin/index.php?route=common/login');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('demo');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('demo');
+  await page.locator('//button[normalize-space()="Login"]').click();
+  await page.waitForTimeout(2000)
+  await page.locator('.btn-close').hover();
+  await page.locator('.btn-close').click();
+  await page.waitForTimeout(2000)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 test('For iFrames', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/iframe');
   await expect(page.getByRole('heading')).toContainText('An iFrame containing the TinyMCE WYSIWYG Editor');
@@ -11,13 +37,54 @@ test('For iFrames', async ({ page }) => {
 
 test('Web tables/Pagination', async ({ page }) => {
   await page.goto('https://demo.opencart.com/admin/index.php?route=common/login');
-  await expect(page.locator('#form-login div').filter({ hasText: 'Username' })).toBeVisible();
+  //await page.waitForTimeout(10000)
+  //await expect(page.locator('#form-login div').filter({ hasText: 'Username' })).toBeVisible();
   await page.getByPlaceholder('Username').click();
   await page.getByPlaceholder('Username').fill('demo');
   await page.getByPlaceholder('Password').click();
   await page.getByPlaceholder('Password').fill('demo');
-  await page.getByRole('button', { name: ' Login' }).click();
-  await page.getByRole('button').nth(1).click();
-  await page.getByRole('link', { name: ' Customers ' }).click();
-  await page.getByRole('link', { name: 'Customers' }).click();
+  await page.locator('//button[normalize-space()="Login"]').click();
+  await page.waitForTimeout(2000)
+  await page.locator('.btn-close').hover();
+  await page.locator('.btn-close').click();
+  await page.waitForTimeout(2000)
+
+  const expanded = await page.locator('#menu-customer > a').getAttribute('class');
+  if (expanded?.includes("collapsed")) {
+      await page.locator('#menu-customer > a').click();
+  }
+  await page.locator('#menu-customer > ul a[href*="customer&"]').click();
+  await page.waitForLoadState();
+
+
+  let page_count = 1;
+  let max_page_count = 2;
+
+  while (page_count <= max_page_count) {
+
+    if (page_count != 1) {
+
+      await page.locator('//a[normalize-space()=">"]').click();
+    }
+    await page.waitForSelector('#customer tbody > tr');
+
+   
+    const rows = await page.locator('#customer tbody > tr').all();
+
+    // console.log(rows.length)
+
+    let data_count = 1;
+    for (const row of rows) {
+
+      console.log(`Page count - ${page_count} : Row count - ${data_count} | ${await row.locator('td').nth(1).textContent()} | ${await row.locator('td').nth(2).textContent()} | ${await row.locator('td').nth(3).textContent()} | ${await row.locator('td').nth(4).textContent()}`);
+
+      data_count++;
+    }
+    await page.waitForTimeout(2000);
+    page_count++;
+  }
+
+  await page.waitForTimeout(3000);
+  await page.pause();
+
 });
