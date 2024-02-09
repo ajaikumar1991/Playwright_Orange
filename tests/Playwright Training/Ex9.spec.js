38250fa14b283ddff96118152a8c7ebe80508414
ajaikumar1991 <kb.ajaikumar1991@gmail.com>
2024-02-09 18:50:23 +0530

    commit

--- tests/Playwright Training/Ex9.spec.js
+++ tests/Playwright Training/Ex9.spec.js
@@ -1,31 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('Print Tables in Pagination', async ({ page }) => {
-  await page.goto('https://demo.opencart.com/admin/index.php?route=common/login');
-  await page.getByPlaceholder('Username').click();
-  await page.getByPlaceholder('Username').fill('demo');
-  await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').fill('demo');
-  await page.locator('//button[normalize-space()="Login"]').click();
-  await page.waitForTimeout(2000)
-  await page.locator('.btn-close').hover();
-  await page.locator('.btn-close').click();
-  await page.waitForTimeout(2000)
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 test('For iFrames', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/iframe');
   await expect(page.getByRole('heading')).toContainText('An iFrame containing the TinyMCE WYSIWYG Editor');
@@ -37,8 +11,6 @@ test('For iFrames', async ({ page }) => {
 
 test('Web tables/Pagination', async ({ page }) => {
   await page.goto('https://demo.opencart.com/admin/index.php?route=common/login');
-  //await page.waitForTimeout(10000)
-  //await expect(page.locator('#form-login div').filter({ hasText: 'Username' })).toBeVisible();
   await page.getByPlaceholder('Username').click();
   await page.getByPlaceholder('Username').fill('demo');
   await page.getByPlaceholder('Password').click();
@@ -50,28 +22,26 @@ test('Web tables/Pagination', async ({ page }) => {
   await page.waitForTimeout(2000)
 
   const expanded = await page.locator('#menu-customer > a').getAttribute('class');
+
   if (expanded?.includes("collapsed")) {
-      await page.locator('#menu-customer > a').click();
+    await page.locator('#menu-customer > a').click();
   }
+
+
   await page.locator('#menu-customer > ul a[href*="customer&"]').click();
   await page.waitForLoadState();
 
-
   let page_count = 1;
-  let max_page_count = 2;
+  let max_page_count = 5;
 
-  while (page_count <= max_page_count) {
+  while (page_count < max_page_count) {
 
-    if (page_count != 1) {
+    if (page_count != 5) {
 
       await page.locator('//a[normalize-space()=">"]').click();
     }
     await page.waitForSelector('#customer tbody > tr');
-
-   
-    const rows = await page.locator('#customer tbody > tr').all();
-
-    // console.log(rows.length)
+    const rows = await page.locator('#customer tbody > tr').all()
 
     let data_count = 1;
     for (const row of rows) {
